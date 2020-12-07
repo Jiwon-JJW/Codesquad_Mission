a3763084288263a616951aa221b1fa145237b957
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-12-07 16:34:50 +0900

    Cube void method finish

--- Step-1/CubeVoid.java
+++ Step-1/CubeVoid.java
@@ -1,27 +1,25 @@
 public class CubeVoid {
 
-    public void directionL(String direction, int num){
-        if(direction.equalsIgnoreCase("l")){
+    public void directionL(int num, String[] arr){
             if(num >0){
-
+                wordChangeL(arr,num);
             }
 
             if(num <0){
-
+                num = num*-1;
+                wordChangeR(arr,num);
             }
-        }
     }
 
-    public void directionR(String direction, int num){
-        if(direction.equalsIgnoreCase("r")){
+    public void directionR(int num, String[] arr){
             if(num >0){
-
+                wordChangeR(arr,num);
             }
 
             if(num <0){
-
+                num = num*-1;
+                wordChangeL(arr,num);
             }
-        }
     }
 
     public void wordChangeL(String[] arr,int num){
@@ -40,14 +38,11 @@ public class CubeVoid {
 
     public void wordChangeR(String[] arr, int num){
         for(int i = 0; i< num;i++){
-            String temp = arr[0];
-            for(int j = 0; j< arr.length; j++){
-                if(j == arr.length-1){
-                   arr[arr.length-1] = temp;
-                   break;
-                }
-                arr[j]= arr[j+1];
+            String temp = arr[arr.length-1];
+            for(int j = arr.length-1; j>0; j--){
+                arr[j]= arr[j-1];
             }
+            arr[0] = temp;
         }
 
         for(int i = 0; i<arr.length; i++){
