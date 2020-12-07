8c9ed4b4bcb0f3333b7db381c5ea7266cd4aadf6
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-12-07 16:12:47 +0900

    Main method Edit

--- Step-1/Cube.java
+++ Step-1/Cube.java
@@ -3,30 +3,12 @@ import java.util.Scanner;
 public class Cube {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        CubeVoid cubeVoid= new CubeVoid();
 
-        StringBuffer sb = new StringBuffer(scanner.next());
+        String word = scanner.next();
         int num = scanner.nextInt();
         String direction = scanner.next();
 
-        if(sb.toString().equalsIgnoreCase("l")){
-            if(num >0){
-
-            }
-
-            if(num <0){
-
-            }
-        }
-
-        if(sb.toString().equalsIgnoreCase("r")){
-            if(num >0){
-
-            }
-
-            if(num <0){
-
-            }
-        }
-
+        String[] wordArr = word.split("");
     }
 }
