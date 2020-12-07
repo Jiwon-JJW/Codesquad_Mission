69376163e6fa3d45378d23d9ad713436e3c7e7d6
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-12-07 16:34:30 +0900

    Main method finish

--- Step-1/Cube.java
+++ Step-1/Cube.java
@@ -5,10 +5,20 @@ public class Cube {
         Scanner scanner = new Scanner(System.in);
         CubeVoid cubeVoid= new CubeVoid();
 
+
+        System.out.print("> ");
+
         String word = scanner.next();
         int num = scanner.nextInt();
         String direction = scanner.next();
 
         String[] wordArr = word.split("");
+
+        if(direction.equalsIgnoreCase("l")){
+            cubeVoid.directionL(num, wordArr);
+        }
+        if(direction.equalsIgnoreCase("r")){
+            cubeVoid.directionR(num, wordArr);
+        }
     }
 }
