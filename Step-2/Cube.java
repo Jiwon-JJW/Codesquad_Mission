0a1d085d9401543baefff5ff48fa897a576dfc84
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-12-09 16:48:05 +0900

    메인 간소화 진행

--- Step-2/Cube.java
+++ Step-2/Cube.java
@@ -1,14 +1,9 @@
-import java.util.Scanner;
-
 public class Cube {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         CubeVoid cubeVoid = new CubeVoid();
 
-        cubeVoid.printCube();
-        System.out.print("CUBE >");
-        String command = scanner.next();
-        cubeVoid.inputCommand(command);
+        cubeVoid.printMain();
 
     }
 }
