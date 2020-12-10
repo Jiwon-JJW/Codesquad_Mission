5ac47ac86b869d555f455920f9f7894655a1c321
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-12-10 18:54:09 +0900

    3차원 배열로 배열 변경

--- Step-3/CubePrint.java
+++ Step-3/CubePrint.java
@@ -1,50 +1,40 @@
 public class CubePrint {
-    String[] w = {"W","W","W","W","W","W","W","W","W"};
-    String[] o = {"O","O","O","O","O","O","O","O","O"};
-    String[] b = {"B","B","B","B","B","B","B","B","B"};
-    String[] r = {"R","R","R","R","R","R","R","R","R"};
-    String[] g = {"G","G","G","G","G","G","G","G","G"};
-    String[] y = {"Y","Y","Y","Y","Y","Y","Y","Y","Y"};
+    String[][][] arr = {
+            {{"B", "B", "B"}, {"B", "B", "B"}, {"B", "B", "B"}},
+            {{"W", "W", "W"}, {"W", "W", "W"}, {"W", "W", "W"}},
+            {{"O", "O", "O"}, {"O", "O", "O"}, {"O", "O", "O"}},
+            {{"G", "G", "G"}, {"G", "G", "G"}, {"G", "G", "G"}},
+            {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}},
+            {{"R", "R", "R"}, {"R", "R", "R"}, {"R", "R", "R"}},
+    };
 
-    public void firstPrint(int n){
-        for(int i = n; i < n+3; i++){
-            System.out.printf("%2s",w[i]);
+    public void print2(){
+        for (int j = 0; j < arr[0].length; j++) {
+            for (int i = 1; i < arr.length - 1; i++) {
+                for (int k = 0; k < 3; k++) {
+                    System.out.printf("%2s", arr[i][1][k]);
+                }
+                System.out.printf("%6s", " ");
+            }
+            System.out.println();
         }
-        System.out.printf("%6s"," ");
-
-        for(int i = n; i < n+3; i++){
-            System.out.printf("%2s",o[i]);
-        }
-        System.out.printf("%6s"," ");
-
-        for(int i = n; i < n+3; i++){
-            System.out.printf("%2s",g[i]);
-        }
-        System.out.printf("%6s"," ");
-
-        for(int i = n; i < n+3; i++){
-            System.out.printf("%2s",y[i]);
-        }
-        System.out.println();
     }
 
-    public void blankPrint(String[] arr){
-        for(int i = 1; i<= arr.length; i++){
-            if(i-1 == 0 || i == 4 || i ==7){
-                System.out.printf("%18s"," ");
+    public void print1(int n){
+        for (int j = 0; j < arr[0].length; j++) {
+            for (int k = 0; k < arr[0][0].length; k++) {
+                if (k == 0 || k == 4 || k == 7) {
+                    System.out.printf("%18s", " ");
+                }
+                System.out.printf("%2s", arr[n][j][k]);
             }
-            System.out.printf("%2s",arr[i-1]);
-            if(i%3 ==0){
-                System.out.println();
-            }
+            System.out.println();
         }
     }
 
     public void printCube(){
-        blankPrint(b);
-        firstPrint(0);
-        firstPrint(3);
-        firstPrint(6);
-        blankPrint(r);
+        print1(0);
+        print2();
+        print1(5);
     }
 }
